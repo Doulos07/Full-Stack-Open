90ca5c75716ee36df935635b9dd8ec9e9b9cd80e
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-05-22 15:54:51 -0300

    Excersice 2.6

---
+++ part2/form/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    const newperson = {
+      id: persons.length + 1,
+      name: newName,
+    };
+    setPersons(persons.concat(newperson));
+    setNewName("");
+  };
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name:
+          <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>debug: {newName}</div>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => {
+          return <Person key={person.id} person={person} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
