b5ce771f758c930cd1cdb8f7537a945421a38a65
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-05-23 14:02:43 -0300

    Excersice 2.11

--- part2/form/src/App.jsx
+++ part2/form/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, useEffectEvent } from "react";
 import Person from "./components/Person";
+import axios from "axios";
 
 const Filter = ({ filter, handleChange }) => {
   return (
@@ -51,15 +52,18 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const dataList = persons.filter((person) =>
     person.name.toLowerCase().includes(filter.toLowerCase()),
