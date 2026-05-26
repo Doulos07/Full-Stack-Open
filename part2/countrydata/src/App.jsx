689f183c1717eadba90f332793d5d9403bc68899
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-05-26 15:24:47 -0300

    arrangements

--- part2/countrydata/src/App.jsx
+++ part2/countrydata/src/App.jsx
@@ -1,70 +1,64 @@
-import { useState, useEffectEvent, useEffect, use } from "react";
-import countrySerives from "./services/countrys";
+import { useState, useEffect } from "react";
+import countryServices from "./services/countrys";
 import Countrys from "./components/Countrys";
 import Country from "./components/Country";
 
-const SearchCountry = ({ search, handleChange }) => {
-  return (
-    <>
-      <form>
-        <div>
-          <input value={search} onChange={handleChange} />
-        </div>
-      </form>
-    </>
-  );
-};
+const SearchCountry = ({ search, handleChange }) => (
+  <form>
+    <input value={search} onChange={handleChange} />
+  </form>
+);
 
 const App = () => {
   const [countrys, setCountrys] = useState([]);
   const [search, setSearch] = useState("");
-  const [selectCountry, setSelectCountry] = useState(null);
+  const [selectedName, setSelectedName] = useState(null);
 
-  let countryList = countrys.filter((country) =>
+  useEffect(() => {
+    countryServices.getAll().then(setCountrys);
+  }, []);
+
+  const countryList = countrys.filter((country) =>
     country.name.common.toLowerCase().includes(search.toLowerCase()),
   );
 
-  if (countryList.length === 1) {
-    let name = countryList[0].name.common.toLowerCase();
-    countrySerives
-      .getCountry(name)
-      .then((response) => setSelectCountry(response));
-  }
+  const filterCountry =
+    selectedName ??
+    (countryList.length === 1 ? countryList[0].name.common : null);
+
+  const [activeCountry, setActiveCountry] = useState(null);
 
   useEffect(() => {
-    countrySerives.getAll().then((response) => {
-      setCountrys(response);
-    });
-  }, []);
+    if (!filterCountry) {
+      setActiveCountry(null);
+    } else {
+      countryServices
+        .getCountry(filterCountry.toLowerCase())
+        .then(setActiveCountry);
+    }
+  }, [filterCountry]);
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
-    setSelectCountry(null);
+    setSelectedName(null);
   };
 
   const handleSelectCountry = (event) => {
-    const name = event.target.value;
-    console.log("name:", name);
-    countrySerives.getCountry(name).then((response) => {
-      console.log("response:", response);
-      setSelectCountry(response);
-    });
+    setSelectedName(event.target.value);
   };
 
-  console.log("select:", selectCountry);
   return (
     <div>
-      <h1>Countrys</h1>
+      <h1>Countries</h1>
       <SearchCountry search={search} handleChange={handleSearch} />
 
-      {selectCountry && <Country country={selectCountry} />}
-
-      {!selectCountry &&
-        (search && countryList.length <= 10 ? (
-          <Countrys countrys={countryList} handle={handleSelectCountry} />
-        ) : (
-          <p>Too many matches, specify another filter</p>
-        ))}
+      {activeCountry ? (
+        <Country country={activeCountry} />
+      ) : search && countryList.length <= 10 ? (
+        <Countrys countrys={countryList} handle={handleSelectCountry} />
+      ) : (
+        <p>Too many matches, specify another filter</p>
+      )}
     </div>
   );
 };
